62bfa143b1a8a54818b4349e72dec0c93bbe9c63
thosapol <isocare.thospol@gmail.com>
2018-11-21 12:15:04 +0700

    Add func deleteData()

--- main.go
+++ main.go
@@ -18,10 +18,10 @@ func main() {
 	connectDB()
 	//selectAll(db)
 	insertstmt = "INSERT INTO users (firstname, lastname, email,tel) values ($1, $2,$3,$4)"
-	firstname = "udom"
-	lastname = "neakeaw"
-	email = "udom@gmail.com"
-	tel = "0954236155"
+	firstname = "aekawan"
+	lastname = "krubsun"
+	email = "aekawan@gmail.com"
+	tel = "0897979696"
 	insertData(db, firstname, lastname, email, tel, insertstmt)
 
 }
@@ -70,5 +70,20 @@ func insertData(db *sql.DB, firstname, lastname, email, tel, insertstmt string) 
 		log.Fatal(err)
 	}
 	fmt.Println("result Number Row of Insert:", numberOfInsert)
+	selectAll(db)
+}
+
+func deleteData(db *sql.DB, id int) {
+	deleteStmt := "delete from users where id = $1"
+	result, err := db.Exec(deleteStmt, id)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	numberOfDeleteRow, err := result.RowsAffected()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("result Number Row of Delete:", numberOfDeleteRow)
 	selectAll(db)
 }
